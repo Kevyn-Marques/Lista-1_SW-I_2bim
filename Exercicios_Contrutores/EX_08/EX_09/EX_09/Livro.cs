a60e15212b53c1c7b61fefa100413a18bf3b8fdf
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-05-17 11:22:21 -0300

    Add files via upload

---
+++ Exercicios_Contrutores/EX_08/EX_09/EX_09/Livro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_09
+{
+    public class Livro
+    {
+        private string titulo;
+        private string autor;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    //Estabelece um padrão de:Caso o valor seja nulo ou esteja em branco,dispara a mensagem de erro
+                    throw new ArgumentException("O título não pode estar vazio.");
+                }
+                titulo = value;
+            }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    //Estabelece um padrão de:Caso o valor seja nulo ou esteja em branco,dispara a mensagem de erro
+                    throw new ArgumentException("O autor não pode estar vazio.");
+                }
+                autor = value;
+            }
+        }
+
+        // Construtor da classe Livro
+        public Livro(string titulo, string autor)
+        {
+            Titulo = titulo;
+            Autor = autor;
+        }
+    }
+}
+
---
+++ Exercicios_Contrutores/EX_08/EX_09/EX_09/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EX_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso da classe Livro
+            try
+            {
+                Livro livro = new Livro("A Moreninha", "Joaquim Manoel de Macedo");
+
+                Console.WriteLine($"Título: {livro.Titulo}");
+                Console.WriteLine($"Autor: {livro.Autor}");
+            }
+            //Capta os padrões estabelecidos no Livro.cs
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
