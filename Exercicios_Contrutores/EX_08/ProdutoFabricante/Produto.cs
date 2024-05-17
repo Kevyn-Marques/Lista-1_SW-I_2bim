578b02c259c62b47525ee53351c3f010d86de826
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-05-17 11:20:04 -0300

    Add files via upload

---
+++ Exercicios_Contrutores/EX_08/ProdutoFabricante/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Fabricante
+{
+    public string Nome { get; set; }
+    public string Endereco { get; set; }
+    public string Cidade { get; set; }
+
+    // Construtor da classe Fabricante
+    public Fabricante(string nome, string endereco, string cidade)
+    {
+        Nome = nome;
+        Endereco = endereco;
+        Cidade = cidade;
+    }
+}
+
+ public class Produto
+{
+    public string Nome { get; set; }
+    public Fabricante Fabricante { get; set; }
+   
+    private decimal preco;
+
+    public decimal Preco
+    {
+        get { 
+            return preco; 
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                preco = value;
+            }
+            else
+            {
+                //Estabelece um padrão de:Caso o valor negativo,será lançado(throw) a mensagem de erro
+                throw new ArgumentException("O preço deve ser positivo.");
+            }
+        }
+    }
+
+    // Construtor da classe Produto
+    public Produto(string nome, Fabricante fabricante, decimal preco)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            //Estabelece um padrão de:Caso o valor seja nulo ou esteja em branco,será lançado(throw) a mensagem de erro
+            throw new ArgumentException("O nome não pode estar vazio.");
+        }
+
+        Nome = nome;
+        Fabricante = fabricante;
+        Preco = preco;
+    }
+}
---
+++ Exercicios_Contrutores/EX_08/ProdutoFabricante/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso das classes Fabricante e Produto
+            try
+            {
+                Fabricante fabricante = new Fabricante("Xiomi", "Rua João Pavani, 123", "Ribeirão Pires");
+                Produto produto = new Produto("Celular", fabricante, 1450.59m);
+
+                Console.WriteLine($"Nome do Produto: {produto.Nome}");
+                Console.WriteLine($"Fabricante: {produto.Fabricante.Nome}");
+                Console.WriteLine($"Preço: R${produto.Preco}");
+            }
+            //Capta os padrões estabelecidos no Produto.cs
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
